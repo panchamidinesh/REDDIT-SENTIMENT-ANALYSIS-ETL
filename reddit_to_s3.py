b6ff519f3a63eee9031af6475763f4cc2a03930d
panchamidinesh <155731989+panchamidinesh@users.noreply.github.com>
2025-10-29 14:24:48 +0530

    Update reddit_to_s3.py

--- reddit_to_s3.py
+++ reddit_to_s3.py
@@ -7,9 +7,9 @@ from datetime import datetime
 @task(retries=3, retry_delay_seconds=10)
 def fetch_reddit_posts(limit=200):
     reddit = praw.Reddit(
-        client_id="2w-k5CLwVHoO0etZmeqzxQ",
-        client_secret="mDP_AIey2zdDBM3TLiQQXrpnB3yOaw",
-        user_agent="airline_sentiment_app:v1.0 (by u/Long-Lifeguard3019)"
+        client_id=" ",
+        client_secret="  ",
+        user_agent="  "
     )
     
     airline_keywords = [
@@ -46,7 +46,7 @@ def fetch_reddit_posts(limit=200):
 @task(retries=2)
 def upload_to_s3(csv_file: str):
     s3 = boto3.client("s3")
-    bucket_name = "fde-twi-bucket1"
+    bucket_name = "YOUR-BUCKET-NAME"
     key = f"reddit_posts/{csv_file}"
     
     with open(csv_file, "rb") as f:
@@ -63,4 +63,5 @@ def reddit_to_s3_flow():
 
 # Optional: run manually
 if __name__ == "__main__":
+
     reddit_to_s3_flow()
